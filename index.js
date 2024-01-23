73826f3cf9c1522906be67083c8a0c7d2c090cfb
Pavle <p.devidze@gmail.com>
2024-01-23 16:58:37 +0400

    update config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { createWorker } from 'tesseract.js';
 
 (async () => {
-    const worker = await createWorker('eng');
+    const config = { load_system_dawg: false, load_freq_dawg: false };
+    const worker = await createWorker('eng', undefined, undefined, config);
     const ret = await worker.recognize('squad.png');
     console.log(ret.data.text);
     await worker.terminate();
