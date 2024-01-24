d75d571bf2358d7786e2c531982482292396ba50
Pavle <p.devidze@gmail.com>
2024-01-24 12:50:45 +0400

    add crop canvas

--- renderer.js
+++ renderer.js
@@ -22,9 +22,11 @@ const config = {
     await worker.terminate();
 })(); */
 
+/** @type {HTMLCanvasElement} */
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
+/** @type {HTMLCanvasElement} */
 const overlayCanvas = document.querySelector('.overlay-canvas');
 const octx = overlayCanvas.getContext('2d');
 const clearOverlay = () => octx.clearRect(0, 0, overlayCanvas.width, overlayCanvas.height);
@@ -33,16 +35,34 @@ let isMouseDown = false;
 
 let rect = { x: 0, y: 0, w: 0, h: 0 };
 function updateRect(newRect) {
-    rect = newRect;
-    octx.strokeStyle = "white";
-    clearOverlay();
-    octx.strokeRect(rect.x, rect.y, rect.w, rect.h)
+    let { x, y, w, h } = newRect;
+    x = Math.floor(x);
+    y = Math.floor(y);
+    w = Math.floor(w);
+    h = Math.floor(h);
+    rect = { x, y, w, h };
+    drawRect();
 }
-
+function drawRect() {
+    let { x, y, w, h } = rect;
+    octx.lineWidth = "1px";
+    octx.strokeStyle = "#FFF";
+    clearOverlay();
+    octx.strokeRect(x, y, w, h);
+}
 function resizeCanvas(w, h) {
     overlayCanvas.width = canvas.width = w;
     overlayCanvas.height = canvas.height = h;
     updateRect({ x: 0, y: 0, w, h });
+}
+function getNormalRect() {
+    const { x, y, w, h } = rect;
+    return {
+        x: Math.min(x, x + w),
+        w: Math.abs(w),
+        y: Math.min(y, y + h),
+        h: Math.abs(h)
+    };
 }
 
 const shortcuts = {
@@ -59,6 +79,21 @@ const shortcuts = {
         resizeCanvas(width, height);
         const imageData = new ImageData(new Uint8ClampedArray(buffer), width, height);
         ctx.putImageData(imageData, 0, 0);
+    },
+    Enter: () => {
+        console.log(`Enter was pressed`);
+
+        const { x, y, w, h } = getNormalRect();
+        if (w < 1 || h < 1) return alert('Rectangle too small');
+
+        const tempCanvas = document.createElement('canvas');
+        const tctx = tempCanvas.getContext('2d');
+        tempCanvas.width = w;
+        tempCanvas.height = h;
+        tctx.drawImage(canvas, x, y, w, h, 0, 0, w, h);
+
+        resizeCanvas(w, h);
+        ctx.drawImage(tempCanvas, 0, 0);
     }
 };
 
