ee0c1ee46557a627482636bdebbe57d2f703110b
Pavle <p.devidze@gmail.com>
2024-01-31 16:49:17 +0400

    separate scrape mult users add scrape loading display dont scrape lines with data remove data on failed lines

--- src/renderer/shortcutsTerminal.js
+++ src/renderer/shortcutsTerminal.js
@@ -4,25 +4,22 @@ import { closeTerminal, getTerminalLines, openTerminal, terminal, writeTerminalL
 export default [
     ['Enter', 'Scrape', async () => {
         try {
-            const unique = arr => Array.from(new Set(arr.filter(Boolean)));
-            const extractUsername = str => {
-                const whitespace = str => str.trim().replace(/\s+/g, ' ');
-                const match = whitespace(str).match(/(.*?)(\s+-\s+\S*)?$/)?.[1];
-                if (!match) return '';
-                const [first, ...rest] = match.split(' ');
-                return rest.join('') || first;
-            };
+            const parsedLines = parseLines(getTerminalLines());
+            if (!parsedLines.length) return window.electron.status("Scrape: EMPTY");
 
-            const lines = unique(getTerminalLines().map(extractUsername));
-            if (!lines.length) return window.electron.status("Scrape: EMPTY");
+            const lines = parsedLines.map(x => fkv(x.username, x.data));
             window.electron.status('Scrape: INIT', lines);
             writeTerminalLines(lines);
 
             window.electron.status('Scrape: START');
-            const initLines = getTerminalLines();
-            await Promise.allSettled(initLines.map(async (line, index) => {
-                const data = await window.electron.scrape(line);
-                const newLine = `${line} - ${data}`;
+            await Promise.allSettled(parsedLines.map(async ({ username, data }, index) => {
+                if (data) return;
+
+                writeTerminalLine(index, fkv(username, '...'));
+
+                const newData = await window.electron.scrape(username);
+
+                const newLine = fkv(username, newData);
                 window.electron.status(`Scrape: ${newLine}`);
                 writeTerminalLine(index, newLine);
             }));
@@ -41,3 +38,44 @@ export default [
         remindShortcuts();
     }]
 ];
+
+function parseLines(lines) {
+    const mappedLines = lines.map(line => {
+        const [username, data] = parseUser(line);
+        return { username, data };
+    });
+
+    const filteredLines = mappedLines.filter(x => x.username)
+
+    return unique(filteredLines, x => x.username);
+}
+
+function parseUser(str) {
+    const normal = whitespace(str);
+    const match = normal.match(/^(.*?)((^|\s+)-\s*(\S*))?$/);
+    const user = match?.[1];
+    const data = match?.[4];
+
+    if (!user) return [];
+
+    const [first, ...rest] = user.split(' ');
+    const username = rest.join('') || first;
+
+    return [username, data];
+}
+
+function unique(arr, getKey) {
+    const entries = arr.map((...args) => {
+        const [v] = args;
+        return [getKey(...args), v];
+    });
+    return Array.from(new Map(entries).values());
+}
+
+function fkv(k, v) {
+    return v ? `${k} - ${v}` : k;
+}
+
+function whitespace(str) {
+    return str.trim().replace(/\s+/g, ' ');
+}
