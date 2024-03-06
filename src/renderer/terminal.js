d63c9501cff4b20375a5de1346e9a345bcfe2330
Pavle <p.devidze@gmail.com>
2024-03-06 14:07:34 +0400

    extract terminal logHistory file

--- src/renderer/terminal.js
+++ src/renderer/terminal.js
@@ -9,6 +9,7 @@ export * from "./terminal/select.js";
 import { getSelection, setSelection } from "./terminal/select.js";
 export * from "./terminal/value.js";
 import { setValue, getValue, getLines } from "./terminal/value.js";
+export * from "./terminal/logHistory.js";
 
 /** @type {HTMLTextAreaElement} */
 export const element = document.querySelector('textarea.terminal');
@@ -109,48 +110,6 @@ export function parseSnapshot(snapshot = {}, value) {
     const lines = getLines(value);
     const newValue = lines.join('\n')
     return { size, start, end, dir, entries, lines, value: newValue };
-}
-
-export function logHistory() {
-    const operation = ([row, text]) => `${row}=${text.length === 1 ? text : `"${text}"`}`;
-    const indent = ' '.repeat(2);
-    const sel = ({ start, end, dir }) => {
-        const diff = JSON.stringify(start) !== JSON.stringify(end);
-        const list = [start, diff && end, dir?.[0]];
-        return list.filter(Boolean).join('-');
-    };
-    const snapshot = (s, i) => {
-        const { size, start, end, dir, entries } = parseSnapshot(s);
-        const selStr = sel({ start, end, dir });
-        const entriesStr = entries.map(operation).join(' ');
-        return `${i}: size=${size} sel=${selStr} ${entriesStr}`;
-    };
-    const value = JSON.stringify(getValue());
-    const valueStr = indent + `value: ${value}`;
-
-    const selStr = indent + `sel: ${sel(getSelection())}`;
-
-    const committed = JSON.stringify(getValue(true));
-    const committedStr = value !== committed && (indent + `committed: ${committed}`);
-
-
-    const base = `#: ${JSON.stringify(state.historyBase)}`;
-    const historyStr = [base, ...history].map((s, i) => {
-        const mid = --i === state.historyIndex ? '> ' : '  ';
-        const end = i < 0 ? base : snapshot(s, i);
-        return `${indent}${mid}${end}`;
-    }).join('\n');
-
-    const historyIndexStr = indent + `historyIndex: ${state.historyIndex}`;
-
-    const lockedKeys = Array.from(lockedLines.keys());
-    const locked = lockedKeys.length > 0 && (indent + `locked: ${lockedKeys}`);
-
-    const logs = [
-        `History:`, historyStr, historyIndexStr,
-        valueStr, committedStr, selStr, locked
-    ];
-    console.log(logs.filter(Boolean).join('\n'));
 }
 
 export function generateSnapshot(dict, value) {
---
+++ src/renderer/terminal/logHistory.js
@@ -0,0 +1,45 @@
+import { getSelection } from "./select.js";
+import { getValue } from "./value.js";
+import { parseSnapshot, state, history, lockedLines } from "../terminal.js";
+
+export function logHistory() {
+    const operation = ([row, text]) => `${row}=${text.length === 1 ? text : `"${text}"`}`;
+    const indent = ' '.repeat(2);
+    const sel = ({ start, end, dir }) => {
+        const diff = JSON.stringify(start) !== JSON.stringify(end);
+        const list = [start, diff && end, dir?.[0]];
+        return list.filter(Boolean).join('-');
+    };
+    const snapshot = (s, i) => {
+        const { size, start, end, dir, entries } = parseSnapshot(s);
+        const selStr = sel({ start, end, dir });
+        const entriesStr = entries.map(operation).join(' ');
+        return `${i}: size=${size} sel=${selStr} ${entriesStr}`;
+    };
+    const value = JSON.stringify(getValue());
+    const valueStr = indent + `value: ${value}`;
+
+    const selStr = indent + `sel: ${sel(getSelection())}`;
+
+    const committed = JSON.stringify(getValue(true));
+    const committedStr = value !== committed && (indent + `committed: ${committed}`);
+
+
+    const base = `#: ${JSON.stringify(state.historyBase)}`;
+    const historyStr = [base, ...history].map((s, i) => {
+        const mid = --i === state.historyIndex ? '> ' : '  ';
+        const end = i < 0 ? base : snapshot(s, i);
+        return `${indent}${mid}${end}`;
+    }).join('\n');
+
+    const historyIndexStr = indent + `historyIndex: ${state.historyIndex}`;
+
+    const lockedKeys = Array.from(lockedLines.keys());
+    const locked = lockedKeys.length > 0 && (indent + `locked: ${lockedKeys}`);
+
+    const logs = [
+        `History:`, historyStr, historyIndexStr,
+        valueStr, committedStr, selStr, locked
+    ];
+    console.log(logs.filter(Boolean).join('\n'));
+}
