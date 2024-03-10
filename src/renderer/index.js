76fdb9cb1f74580c8ff4317b5e5170a186a90300
Pavle <p.devidze@gmail.com>
2024-03-10 12:48:16 +0400

    add debug tools

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,5 @@
 import { canvasBackground } from "./canvas.js";
-import { moveDrag, startDrag, stopDrag, zoom } from "./mouse.js";
+import { getCanvasMousePos, moveDrag, startDrag, stopDrag, zoom } from "./mouse.js";
 import { resizeCanvas } from "./rect.js";
 import { onKeyDown } from "./shortcuts.js";
 import * as terminal from "../terminal/index.js";
@@ -15,5 +15,14 @@ canvasBackground.addEventListener('mousemove', moveDrag);
 canvasBackground.addEventListener('mouseup', stopDrag);
 canvasBackground.addEventListener('mouseleave', stopDrag);
 window.addEventListener('wheel', zoom);
+
+// DEBUG
+window.addEventListener('mousemove', e => {
+    const [mx, my] = getCanvasMousePos(e);
+    const elt = document.querySelector('.debug-mouse');
+    const mxs = (mx || 0).toString().padEnd(4, ' ');
+    const mys = my || 0;
+    elt.textContent = [mxs, mys].join(' ');
+});
 
 api.onStatus(console.log);
