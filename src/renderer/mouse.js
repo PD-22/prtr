4dfb9c12588117b60c8d6ef3bb3adc66394d1c1d
Pavle <p.devidze@gmail.com>
2024-03-16 09:18:12 +0400

    add new scale mousePos add ease animation remove comments

--- src/renderer/mouse.js
+++ src/renderer/mouse.js
@@ -36,57 +36,27 @@ export function stopDrag() {
 }
 
 export function zoom(dir, mousePos) {
-    const sign = dir ? 1 : -1;
-
-    const zoomStep = 2;
-    const zoomNum = 3
-    const maxScale = zoomStep ** zoomNum;
-    const minScale = 1 / maxScale;
-    const numScale = zoomStep ** sign;
-
-    // const initWidth = parseInt(canvas.width || 0);
-    // const initHeight = parseInt(canvas.height || 0);
-    // const eltWidth = parseInt(canvas.style.width || initWidth);
-    // const eltHeight = parseInt(canvas.style.height || initHeight);
-    // const { innerWidth, innerHeight } = window;
-    // const [scrollX, scrollY] = getScroll();
-    // const [mx, my] = mousePos ?? [];
+    const step = 2;
+    const limit = 3;
 
     const scale = getScale();
-    // const widthScale = eltWidth / initWidth;
-    // const heightScale = eltHeight / initHeight;
-    const newScale = clamp(scale * numScale, minScale, maxScale)
-    // const newWidthScale = clamp(widthScale * numScale, minScale, maxScale);
-    // const newHeightScale = clamp(heightScale * numScale, minScale, maxScale);
-    // const newWidth = initWidth * newWidthScale;
-    // const newHeight = initHeight * newHeightScale;
-
-    // const centerX = eltWidth / 2 + scrollX;
-    // const centerY = eltHeight / 2 + scrollY;
-    // const tx = mx == null ? centerX : mx * widthScale;
-    // const ty = my == null ? centerY : my * heightScale;
-    // const dx = centerX - (centerX - tx) * (1 - eltWidth / newWidth) - eltWidth / 2;
-    // const dy = centerY - (centerY - ty) * (1 - eltHeight / newHeight) - eltHeight / 2;
+    const newScale = clamp(
+        scale * step ** (dir ? 1 : -1),
+        step ** -limit,
+        step ** limit
+    );
 
     if (newScale === scale) return;
-    // if (newWidthScale === widthScale || !newWidth) return;
-    // if (newHeightScale === heightScale || !newHeight) return;
 
     setScale(newScale);
-    // const styleWidth = `${Math.floor(newWidth)}px`;
-    // const styleHeight = `${Math.floor(newHeight)}px`;
-    // // canvasContainer.style.width = styleWidth;
-    // // canvasContainer.style.height = styleHeight;
-    // // canvas.style.width = styleWidth;
-    // // canvas.style.height = styleHeight;
-    // // overlayCanvas.style.width = styleWidth;
-    // // overlayCanvas.style.height = styleHeight;
 
-    const ds = newScale / scale;
+    const sr = newScale / scale;
     const [x, y] = getScroll();
-    setScroll(x * ds, y * ds);
-    // const widthOverflow = newWidth > innerWidth && innerWidth > eltWidth;
-    // const heightOverflow = newHeight > innerHeight && innerHeight > eltHeight;
-    // if (widthOverflow || heightOverflow) return;
-    // scrollBy(dx * newWidth / eltWidth, dy * newHeight / eltHeight);
+    setScroll(x * sr, y * sr);
+
+    if (!mousePos) return;
+    const mx = mousePos[0] - canvas.width / 2;
+    const my = mousePos[1] - canvas.height / 2;
+    const sd = newScale - scale;
+    setScroll(x + mx * sd, y + my * sd);
 }
